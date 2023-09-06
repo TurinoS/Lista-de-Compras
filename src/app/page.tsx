f86d816a6059cb996c89d2b7ae0983ba44b66f19
Paulo Turino <paulo.turino01@gmail.com>
2023-09-06 11:40:32 -0300

    renderizando itens do local storage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,28 @@
+'use client'
+
 import { AiFillStar } from 'react-icons/ai'
 import Header from './components/Header'
 import CheckBox from './components/CheckBox'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+  const [storedItems, setStoredItems] = useState<any[]>([]);
+
+  useEffect(() => {
+    const data = window.localStorage.getItem("items")
+    if (data) {
+      const parsedData = JSON.parse(data);
+      setStoredItems(parsedData);
+    }
+  }, [])
+
   return (
     <main className="min-h-screen">
       <Header />
       <div className='flex flex-col gap-2 py-4 px-1'>
+        {storedItems.map((item, index) => (
+          <CheckBox key={index} name={item.produto} qtt={item.quantidade} />
+        ))}
         <CheckBox name='pão' qtt='6' />
         <CheckBox name='presunto' qtt='300g' />
         <CheckBox name='Queijo' qtt='300g' />
